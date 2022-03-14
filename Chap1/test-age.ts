1c993ca06f9866a9dd1a196fcff63434ea5debd0
Omar <omardraz94@gmail.com>
2022-03-13 20:58:29 -0700

    first commit

---
+++ Chap1/test-age.ts
@@ -0,0 +1,23 @@
+interface User {
+    name: string;
+    age: number;
+}
+
+function canDrive(usr: User) { //Type annotation
+    
+    console.log('user is', usr.name);
+
+    if(usr.age >= 16) {
+        console.log("allow to drive");
+    } else {
+        console.log("do not allow to drive");
+    }
+}
+
+const tom: User = {
+    name: 'tom',
+    age: 25
+
+}
+
+canDrive(tom);
