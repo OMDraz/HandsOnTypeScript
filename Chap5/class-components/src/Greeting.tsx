dad3eb73e8ac690f9045aa94b5b8e91c82a5dc9f
Omar <omardraz94@gmail.com>
2022-03-24 22:47:54 -0700

    small cahnges;

--- Chap5/class-components/src/Greeting.tsx
+++ Chap5/class-components/src/Greeting.tsx
@@ -8,12 +8,40 @@ interface GreetingState {
 }
 
 export default class Greeting extends React.Component<GreetingProps, GreetingState> {
-    constructor(props: GreetingProps) {
-        super(props);
-        this.state = {
-            message: `Hello from, ${props.name}`
+    Component<GreetingProps> {
+        constructor(props: GreetingProps){
+            super(props);
+            this.state = {
+                message: Greeting.getNewMessage(props.name)
+            }
+            state: GreetingState;
         }
-    }
+
+
+    static getDerivedStateFromProps(props: GreetingProps,
+        state:GreetingState) {
+          console.log(props, state);
+          if(props.name && props.name !== state.message) {
+              const newState = {...state};
+              newState.message =
+                Greeting.getNewMessage(props.name);
+              return newState;
+          }
+          return state;
+      }
+      static getNewMessage(name: string = "") {
+          return `Hello from, ${name}`;
+      }
+      render() {
+          console.log("rendering Greeting")
+          if(!this.props.name) {
+              return <div>no name given</div>;
+          }
+          return <div>
+              {this.state.message}
+          </div>;
+      }
+  }
 
     render() {
         if(!this.props.name) {
