5d799d08e63a5b16d98c86ef2b51dbfb65d222f6
Omar <omardraz94@gmail.com>
2022-03-30 18:53:39 -0700

    finished work on testing

--- Chap6/ejected-app/src/DisplayText.tsx
+++ Chap6/ejected-app/src/DisplayText.tsx
@@ -7,6 +7,7 @@ interface DisplayTextProps {
 const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
   const [txt, setTxt] = useState("");
   const [msg, setMsg] = useState("");
+  const [todos, setTodos] = useState<Array<JSX.Element>>();
 
   const onChangeTxt = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTxt(e.target.value);
@@ -16,8 +17,33 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
   ) => {
     e.preventDefault();
     setMsg(`Welcome to React testing, ${await getUserFullname(txt)}`);
+    setUsersTodos();
   };
 
+  const setUsersTodos = async () => {
+    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+    if(usersResponse.ok) {
+        const users = await usersResponse.json();
+        const userByName = users.find((usr: any) => {
+            return usr.username.toLowerCase() === txt;
+        });
+        console.log("user by username", userByName);
+      };
+    const todosResponse = await fetch('https://jsonplaceholder.typicode.com/todos');
+     if(todosResponse.ok) {
+         const todos = await todosResponse.json();
+         const usersTodos = todos.filter((todo: any) => {
+             return todo.userId === userByName.id;
+         });
+         const todoList = usersTodos.map((todo: any) => {
+             return <li key={todo.id}>{todo.title}</li>
+         });
+         setTodos(todoList);
+         console.log("user todos", usersTodos);
+     }
+    }
+  }
+      
   return (
     <form>
       <div>
@@ -34,6 +60,7 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
       <div>
         <label data-testid="final-msg">{msg}</label>
       </div>
+      <ul style={{marginTop: '1rem', listStyleType:'none'}}>{todos}</ul>
     </form>
   );
 };
