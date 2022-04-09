b0e0f3a9928aef8e58612d11d74239bbc71c7319
Omar <omardraz94@gmail.com>
2022-04-08 19:50:35 -0700

    learned about graphql, resolvers and typedefs

---
+++ Chap10/gql-server/src/server.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
+
+
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  // Same ApolloServer initialization as before
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  // Required logic for integrating with Express
+  await server.start();
+
+  const app = express();
+
+  server.applyMiddleware({
+     app,
+
+     // By default, apollo-server hosts its GraphQL endpoint at the
+     // server root. However, *other* Apollo Server packages host it at
+     // /graphql. Optionally provide this to match apollo-server.
+     path: '/graphql'
+  });
+
+  // Modified server startup
+  await new Promise<void>(resolve => {
+      return app.listen({ port: 8000 }, resolve);
+  });
+  console.log(`🚀 Server ready at http://localhost:8000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
